decb71320f2beeeff3d1388dcfa9bbe8a2f411ac
ray <ray.g@gmx.de>
2016-01-10 20:44:56 +0100

    create dummy table + user url

--- iot-frontend/lib/app/controller/test.js
+++ iot-frontend/lib/app/controller/test.js
@@ -2,8 +2,16 @@ var User = require('../models/user');
 
 module.exports = me = {};
 
-me.createTable = function(req, res) {
+me.createTable = function(req, res, next) {
   User.createTable('Users', function(err, data) {
+    //if (err) res.json(err);
+    next();
+    //res.json(data);
+  });
+};
+
+me.createDummyUser = function(req, res) {
+  User.createDummyUser(function(err, data) {
     if (err) res.json(err);
     res.json(data);
   });
