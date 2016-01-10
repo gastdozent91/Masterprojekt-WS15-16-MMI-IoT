4ad76c84f56afb2d4656ae2918f87f999e85e456
ray <ray.g@gmx.de>
2016-01-10 20:21:23 +0100

    add semi-functional live switch

--- iot-frontend/lib/public/react/Sensor.jsx
+++ iot-frontend/lib/public/react/Sensor.jsx
@@ -5,16 +5,38 @@ var Sensor = React.createClass({
 
   getInitialState: function() {
     return {
+      isLive: false
     };
   },
 
   componentDidMount: function() {
   },
 
+  handleSwitch: function() {
+    var isLive = !this.state.isLive;
+    console.log(isLive);
+    this.setState({isLive: isLive});
+  },
+
   render: function() {
     return (
       <div>
         <TopBar user={this.props.user} />
+        <div style={{textAlign: 'center', marginTop: 20}}>
+          <p style={{marginBottom: 0}}>Is it live data?</p>
+          <div className="switch large">
+            <input className="switch-input"
+              id="yes-no"
+              type="checkbox"
+              onClick={this.handleSwitch}
+              name="exampleSwitch"/>
+            <label className="switch-paddle" htmlFor="yes-no">
+              <span className="show-for-sr">Do you like me?</span>
+              <span className="switch-active" aria-hidden="true">Yes</span>
+              <span className="switch-inactive" aria-hidden="true">No</span>
+            </label>
+          </div>
+        </div>
       </div>
     );
   }
