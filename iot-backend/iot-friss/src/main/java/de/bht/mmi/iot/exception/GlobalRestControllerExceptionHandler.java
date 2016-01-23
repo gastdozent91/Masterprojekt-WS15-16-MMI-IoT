d3b2022f977b513a22b0acb92f917110f9615482
Steven Maasch <steven.maasch@gmx.de>
2016-01-23 19:51:47 +0100

    Log api exception and update postman collection

--- iot-backend/iot-friss/src/main/java/de/bht/mmi/iot/exception/GlobalRestControllerExceptionHandler.java
+++ iot-backend/iot-friss/src/main/java/de/bht/mmi/iot/exception/GlobalRestControllerExceptionHandler.java
@@ -1,37 +1,20 @@
 package de.bht.mmi.iot.exception;
 
-import org.springframework.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalRestControllerExceptionHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({EntityNotFoundException.class, UsernameNotFoundException.class})
-    public @ResponseBody PublicRestErrorMessage handleEntityNotFound(Exception e) {
-        return new PublicRestErrorMessage(e);
-    }
-
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(EntityExistsException.class)
-    public @ResponseBody PublicRestErrorMessage handleEntityExists(Exception e) {
-        return new PublicRestErrorMessage(e);
-    }
-
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    @ExceptionHandler(AccessDeniedException.class)
-    public @ResponseBody PublicRestErrorMessage handleAccessDenied(Exception e) {
-        return new PublicRestErrorMessage(e);
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalRestControllerExceptionHandler.class);
 
     @ExceptionHandler(AbstractApiException.class)
     public @ResponseBody ResponseEntity<PublicRestErrorMessage> handleAbstractApiException(AbstractApiException e) {
+        LOGGER.warn(e.getMessage());
         return new ResponseEntity<PublicRestErrorMessage>(new PublicRestErrorMessage(e), e.getResponseStatusCode());
     }
 
