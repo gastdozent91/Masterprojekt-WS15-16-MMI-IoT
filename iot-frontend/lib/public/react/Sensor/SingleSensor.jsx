dfae65149ee9fbdfbaad3719cb6c7e51751dd6f8
zimtis@t-online.de <zimtis@t-online.de>
2016-01-27 23:18:19 +0100

    slight deuglification of the sensor detail view

--- iot-frontend/lib/public/react/Sensor/SingleSensor.jsx
+++ iot-frontend/lib/public/react/Sensor/SingleSensor.jsx
@@ -11,7 +11,6 @@ var SingleSensor = React.createClass({
       isLive: false,
       fields: [
         'id',
-        'name',
         'owner',
         'attachedGateway'
       ]
@@ -27,13 +26,13 @@ var SingleSensor = React.createClass({
   renderFields: function(){
     var that = this;
     return this.state.fields.map(function(field){
-      var value = that.props.sensor[field] || '';
-      var text;
+      var value = that.props.sensor[field] || 'missing';
+      var text = value;
       return(
-        <div className='row' key={field}>
-          <div className='large-4 columns'>{field}</div>
-          <div className='large-8 columns'>{value}</div>
-        </div>
+        <tr className='row' key={field}>
+          <td>{field}</td>
+          <td>{field === 'owner' ? <a href={"/user/" + value}>{value}</a> : value}</td>
+        </tr>
       );
     });
   },
@@ -43,8 +42,10 @@ var SingleSensor = React.createClass({
     return this.props.sensor['attachedClusters'].map(function(cluster){
       return(
         <div className='row' key={cluster}>
-          <div className='large-12 columns'>{cluster}</div>
-        </div>
+          <a href=''>
+          <div className='large-12 columns selectable-row'>{cluster}</div>
+</a>
+      </div>
       );
     });
   },
@@ -59,28 +60,40 @@ var SingleSensor = React.createClass({
   render: function() {
     console.log(this.props.sensor);
     return (
-      <div>
-        <div style={{textAlign: 'center', marginTop: 20}}>
-          <p style={{marginBottom: 0}}>Is it live data?</p>
-          <div className="switch large">
-            <input className="switch-input"
-              id="yes-no"
-              type="checkbox"
-              onClick={this.handleSwitch}
-              name="exampleSwitch"/>
-            <label className="switch-paddle" htmlFor="yes-no">
-              <span className="show-for-sr"></span>
-              <span className="switch-active" aria-hidden="true">Yes</span>
-              <span className="switch-inactive" aria-hidden="true">No</span>
-            </label>
+      <div style={{marginTop: 25}}>
+        <div className='row column' style={{float: 'none'}}>
+          <div className='callout'>
+              <h3>{this.props.sensor.name}</h3>
+              <table style={{width: '100%'}}>
+                <tbody style={{borderWidth: '0px'}}>
+                  {this.renderFields()}
+                </tbody>
+              </table>
           </div>
         </div>
         <div className='row column' style={{float: 'none'}}>
           <div className='callout'>
-            <h5>{this.props.sensor.name}</h5>
-            {this.renderFields()}
-            <h3>cluster</h3>
+            <h5>attached cluster</h5>
             {this.renderClusters()}
+          </div>
+        </div>
+        <div className='row column' style={{float:'none'}}>
+          <div className='callout'>
+            <div style={{textAlign: 'center'}}>
+              <p style={{marginBottom: 0}}>Show live data</p>
+              <div className="switch large">
+                <input className="switch-input"
+                  id="yes-no"
+                  type="checkbox"
+                  onClick={this.handleSwitch}
+                  name="exampleSwitch"/>
+                <label className="switch-paddle" htmlFor="yes-no">
+                  <span className="show-for-sr"></span>
+                  <span className="switch-active" aria-hidden="true">Yes</span>
+                  <span className="switch-inactive" aria-hidden="true">No</span>
+                </label>
+            </div>
+          </div>
             {this.renderGraph()}
           </div>
         </div>
