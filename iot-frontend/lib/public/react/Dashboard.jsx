b98e4a7029395d5464044cac380c792704ff8347
ray <ray.g@gmx.de>
2016-02-08 22:24:20 +0100

    add some dashboard information

--- iot-frontend/lib/public/react/Dashboard.jsx
+++ iot-frontend/lib/public/react/Dashboard.jsx
@@ -45,9 +45,9 @@ var Dashboard = React.createClass({
     var withNoCluster = 0;
     var active = 0;
     this.props.sensors.forEach(function(sensor) {
-      if (sensor.attachedCluster.length === 0)
+      if (!sensor.attachedCluster)
         withNoCluster++;
-      if (sensor.active)
+      if (sensor.isActive)
         active++;
     });
     return (
@@ -55,6 +55,79 @@ var Dashboard = React.createClass({
         <div>{infoString}</div>
         <div>{withNoCluster + ' sensors are not attached to a cluster'}</div>
         <div>{active + ' sensors are active'}</div>
+      </div>
+    );
+  },
+
+  renderGatewayInfo: function() {
+    var result = 'You have ';
+    if (this.props.gateways.length !== 1)
+      result += this.props.gateways.length + ' gateways';
+    else
+      result += this.props.gateways.length + ' gateway';
+    var noSensors = 0;
+    this.props.gateways.forEach(function(gateway) {
+      var hasSensors = false;
+      this.props.sensors.forEach(function(sensor) {
+        if (!hasSensors) {
+          if (gateway.id === sensor.attachedGateway)
+            hasSensors = true;
+        }
+      });
+      if (!hasSensors) noSensors++;
+    }.bind(this));
+    var hasNoSensors = noSensors + ' of your gateways don\'t have sensors';
+    return (
+      <div>
+        <div>{result}</div>
+        <div>{hasNoSensors}</div>
+      </div>
+    );
+  },
+
+  renderClusterInfo: function() {
+    var result = 'You have ';
+    if (this.props.clusters.length !== 1)
+      result += this.props.clusters.length + ' clusters';
+    else
+      result += this.props.clusters.length + ' cluster';
+    this.props.clusters.sort(function(a, b) {
+      return new Date(b.creationDate) - new Date(a.creationDate);
+    });
+    var last = 'The last cluster was created at: ' +
+      this.props.clusters[0].creationDate;
+    var noSensors = 0;
+    this.props.clusters.forEach(function(cluster) {
+      var hasSensors = false;
+      this.props.sensors.forEach(function(sensor) {
+        if (!hasSensors) {
+          if (cluster.id === sensor.attachedCluster)
+            hasSensors = true;
+        }
+      });
+      if (!hasSensors) noSensors++;
+    }.bind(this));
+    var hasNoSensors = noSensors + ' of your clusters don\'t have sensors';
+    return (
+      <div>
+        <div>{result}</div>
+        <div>{last}</div>
+        <div>{hasNoSensors}</div>
+      </div>
+    );
+  },
+
+  renderUserInfo: function() {
+    var result = 'Logged in as ' + this.props.user.username;
+    var admin;
+    if (this.props.user.isAdmin)
+      admin = 'You are Admin';
+    else
+      admin = 'You are not an Admin';
+    return (
+      <div>
+        <div>{result}</div>
+        <div>{admin}</div>
       </div>
     );
   },
@@ -80,9 +153,7 @@ var Dashboard = React.createClass({
             <div className='dashboard-widget'>
               <div className='title'><a href='/gateways'>Gateways</a><div className='icons'></div></div>
               <div className='body'>
-                <div>{this.getGatewayString()}</div>
-                <div>blablabla</div>
-                <div>blablabla</div>
+                {this.renderGatewayInfo()}
               </div>
             </div>
           </div>
@@ -94,9 +165,7 @@ var Dashboard = React.createClass({
             <div className='dashboard-widget'>
               <div className='title'><a href='/clusters'>Clusters</a><div className='icons'></div></div>
               <div className='body'>
-                <div>{this.getClusterString()}</div>
-                <div>blablabla</div>
-                <div>blablabla</div>
+                {this.renderClusterInfo()}
               </div>
             </div>
           </div>
@@ -104,11 +173,12 @@ var Dashboard = React.createClass({
           {/* Statistic area */}
           <div className='large-6 columns'>
             <div className='dashboard-widget'>
-              <div className='title'>Statistic<div className='icons'></div></div>
+              <div className='title'>User Information
+                <div className='icons'>
+                </div>
+              </div>
               <div className='body'>
-                <div>blablabla</div>
-                <div>blablabla</div>
-                <div>blablabla</div>
+                {this.renderUserInfo()}
               </div>
             </div>
           </div>
